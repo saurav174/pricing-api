9ddc2560d57559b3dc413f0fa21154c7ae5b495a
saurav174 <sksauravkumar100@gmail.com>
2026-07-11 22:54:26 +0700

    added changes in rate_api_client.rb

--- lib/rate_api_client.rb
+++ lib/rate_api_client.rb
@@ -1,19 +1,23 @@
 class RateApiClient
   include HTTParty
+
   base_uri ENV.fetch('RATE_API_URL', 'http://localhost:8080')
-  headers "Content-Type" => "application/json"
+  headers 'Content-Type' => 'application/json'
   headers 'token' => ENV.fetch('RATE_API_TOKEN', '04aa6f42aa03f220c2ae9a276cd68c62')
 
-  def self.get_rate(period:, hotel:, room:)
-    params = {
-      attributes: [
-        {
-          period: period,
-          hotel: hotel,
-          room: room
-        }
-      ]
-    }.to_json
-    self.post("/pricing", body: params)
+  DEFAULT_TIMEOUT_SECONDS = 2
+
+  class << self
+    def get_rates(attributes:)
+      post('/pricing', body: { attributes: attributes }.to_json, timeout: timeout_seconds)
+    end
+
+    private
+
+    def timeout_seconds
+      Integer(ENV.fetch('RATE_API_TIMEOUT_SECONDS', DEFAULT_TIMEOUT_SECONDS))
+    rescue ArgumentError
+      DEFAULT_TIMEOUT_SECONDS
+    end
   end
 end
